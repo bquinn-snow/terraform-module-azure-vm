c0c74520755e356e6f3ccbe4e4bc6e1acd89082d
bquinn-snow <72510331+bquinn-snow@users.noreply.github.com>
2020-10-07 10:42:47 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "hostname" {
+  type = string
+}
+
+variable "reg" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "vmsize" {
+  type = string
+}
